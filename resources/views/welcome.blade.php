731f1dbc40ea7dcab6f94d012698bb886e3935e7
ForceClose31 <forceclose31@gmail.com>
2024-06-20 12:51:05 +0700

    feat: section 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Mashudi Plumbing</title>
 </head>
 
@@ -45,6 +46,49 @@
                 diselesaikan dengan segera.</p>
         </div>
     </section>
+    <section class="sub-2">
+        <div class="text">
+            <h1>PERKHIDMATAN KAMI</h1>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src={{ asset('assets/img/jubin.jpg') }} class="card-img-top" alt="Service 1">
+                        <div class="card-body">
+                            <h5 class="card-title">TILES / JUBIN TANDAS</h5>
+                            <p class="card-text">Melakukan kerja-kerja pemasangan tiles / jubin serta mozac
+                                tandas.Rekabentuk kami moden dan memenuhi citarasa anda.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src={{ asset('assets/img/sinki.jpg') }} class="card-img-top" alt="Service 2">
+                        <div class="card-body">
+                            <h5 class="card-title">SINKI TERSUMBAT</h5>
+                            <p class="card-text">Kami sedia membaiki pulih sinki atau saluran yang tersumbat di rumah
+                                anda, supaya air dapat mengalir seperti biasa.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src={{ asset('assets/img/pipa.jpg') }} class="card-img-top" alt="Service 3">
+                        <div class="card-body">
+                            <h5 class="card-title">PAIP BOCOR</h5>
+                            <p class="card-text">Membaiki dan mengenal pasti masalah paip bocor. Seperti paip sinki,
+                                paip bilik air, paip meter dan sebagainya.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="#" class="btn btn-primary">Learn More</a>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
