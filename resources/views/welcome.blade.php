bf4553e758564e4f7367c320023bad232be1f108
ForceClose31 <forceclose31@gmail.com>
2024-06-20 11:17:33 +0700

    first init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>Mashudi Plumbing</title>
+</head>
+
+<body>
+    <section class="top">
+        <div class="hero">
+            <div class="content">
+                <img src={{ asset('assets/img/img-person.jpg') }} alt="Person">
+                <div class="text">
+                    <p class="exp">PENGALAMAN SELAMA 8 TAHUN</p>
+                    <h2>PROFESSIONAL PLUMBING SERVIS</h2>
+                    <p>Kami menyediakan perkhidmatan baiki/servis/tambah paip di kawasan Bangi, Kajang, Cheras, Sepang,
+                        Puchong, Seri Kembangan, Serdang, Semenyih, Mantin</p>
+                    <hr>
+                    <div class="contact-info">
+                        <i class="fas fa-phone-alt"></i>
+                        <div class="phone">
+                            <p>Hubungi Kami</p>
+                            <p>+60 123-456-789</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
