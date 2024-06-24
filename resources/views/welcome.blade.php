8ec01b1027716b1323ec26bc45c949771d24aab4
ForceClose31 <forceclose31@gmail.com>
2024-06-24 16:31:57 +0700

    feat: all done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Mashudi Plumbing</title>
@@ -23,10 +24,10 @@
                         Puchong, Seri Kembangan, Serdang, Semenyih, Mantin</p>
                     <hr>
                     <div class="contact-info">
-                        <i class="fas fa-phone-alt"></i>
+                        <a href="tel:60172137979" style="text-decoration: none"><i class="fas fa-phone-alt"></i></a>
                         <div class="phone">
                             <p>Hubungi Kami</p>
-                            <p>+60 123-456-789</p>
+                            <p>+60-172-137-979</p>
                         </div>
                     </div>
                 </div>
@@ -36,12 +37,12 @@
     <section class="sub-1">
         <div class="text">
             <h1>PAKAR TUKANG PAIP (PLUMBER) KUALA LUMPUR, SELANGOR & SEREMBAN</h1>
-            <p>Kami di TUKANGPAIPPLUMBER.COM memiliki pekerja yang mempunyai kemahiran dan kepakaran dalam bidang
+            <p>Kami di MASHUDIPLUMBING.COM memiliki pekerja yang mempunyai kemahiran dan kepakaran dalam bidang
                 masing-masing. Kepakaran yang kami berikan kepada pelanggan termasuk elektrikal, plumber, renovation,
                 mengecat rumah, dan pelbagai lagi. Buat masa ini perkhidmatan kami meliputi kawasan Bangi, Kajang,
                 Cheras,
                 Sepang, Puchong, Seri Kembangan, Serdang, Semenyih, Mantin dan kawasan berdekatan.</p>
-            <p>TUKANGPAIPPLUMBER.COM di tubuhkan untuk membantu warga Lembah Klang yang menghadapi masalah-masalah dan
+            <p>MASHUDIPLUMBING.COM di tubuhkan untuk membantu warga Lembah Klang yang menghadapi masalah-masalah dan
                 perlu
                 diselesaikan dengan segera.</p>
         </div>
@@ -84,8 +85,195 @@
                 </div>
             </div>
         </div>
-        <a href="#" class="btn btn-primary">Learn More</a>
+        <a href="#sub-9">Tempah Khidmat Kami</a>
     </section>
+    <section class="sub-3">
+        <div class="container d-flex align-items-start">
+            <img src="{{ asset('assets/img/sub-3.jpeg') }}" alt="Service Image" class="img-fluid sub-3-img mr-4">
+            <div class="text">
+                <h2>KAMI DATANG KE TEMPAT ANDA</h2>
+                <ul>
+                    <li><i class="fas fa-check-circle text-success"></i> Kami beroperasi 7 hari</li>
+                    <li><i class="fas fa-check-circle text-success"></i> Kami datang terus ke rumah anda</li>
+                    <li><i class="fas fa-check-circle text-success"></i> Anda bebas memilih masa temujanji</li>
+                    <li><i class="fas fa-check-circle text-success"></i> Semua alat ganti adalah 100% genuine</li>
+                    <li><i class="fas fa-check-circle text-success"></i> Pekerja yang berpengalaman & terlatih</li>
+                    <li><i class="fas fa-check-circle text-success"></i> Pekerja akan kenal pasti masalah dahulu</li>
+                </ul>
+                <a href="http://wasap.my/60172137979">Hubungi Kami</a>
+            </div>
+        </div>
+    </section>
+    <section class="sub-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 feature">
+                    <div class="icon">💰</div>
+                    <h4>HARGA BERPATUTAN</h4>
+                    <p>Harga kami berpatutan dengan nilai barangan. Yang penting produk berkualiti & terbaik.</p>
+                </div>
+                <div class="col-md-3 feature">
+                    <div class="icon">🔧</div>
+                    <h4>PENGALAMAN LUAS</h4>
+                    <p>Kami berpengalaman luas & terlatih dalam industri kerja tukang serta bertauliah.</p>
+                </div>
+                <div class="col-md-3 feature">
+                    <div class="icon">😊</div>
+                    <h4>TESTIMONI POSITIF</h4>
+                    <p>Anda boleh lihat pelbagai testimoni yang positif telah diberi oleh pelanggan kami.</p>
+                </div>
+                <div class="col-md-3 feature">
+                    <div class="icon">🔧</div>
+                    <h4>ON-SITE SERVIS</h4>
+                    <p>Kami akan terus ke tempat anda untuk proses servis & kenal pasti kerosakkan.</p>
+                </div>
+                <div class="col-md-3 feature">
+                    <div class="icon">📞</div>
+                    <h4>7 HARI SERVIS</h4>
+                    <p>Anda bebas menghubungi & membuat temujanji dgn kami pada bila-bila masa yang anda mahu.</p>
+                </div>
+                <div class="col-md-3 feature">
+                    <div class="icon">🤝</div>
+                    <h4>PEKERJA PERAMAH</h4>
+                    <p>Semua pekerja kami sangat peramah. Anda boleh berinteraksi dengan mereka.</p>
+                </div>
+                <div class="col-md-3 feature">
+                    <div class="icon">📦</div>
+                    <h4>PILIHAN BARANG</h4>
+                    <p>Anda boleh memilih & membuat pilihan barangan anda sendiri. Pilihan di tangan anda.</p>
+                </div>
+                <div class="col-md-3 feature">
+                    <div class="icon">🏅</div>
+                    <h4>PRODUK BERKUALITI</h4>
+                    <p>Semua barangan kami lulus quality inspection serta di beri jaminan yang ditetapkan.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="sub-5">
+        <div class="content-sub-5">
+            <div class="text-sub-5">
+                <h1>WALAU DIMANA PUN ANDA BERADA KAMI TETAP SEDIA MEMBANTU
+                    BANTUAN SEGERA DIMASA SULIT ANDA</h1>
+            </div>
+            <a href="http://wasap.my/60172137979">Hubungi Kami</a>
+        </div>
+    </section>
+    <section class="sub-6">
+        <div class="gallery-item">
+            <a href="#" data-toggle="modal" data-target="#imageModal">
+                <video width="400" height="550" controls>
+                    <source src="{{ asset('assets/video/video-2.mp4') }}" type="video/mp4">
+                </video>
+            </a>
+        </div>
+        <div class="gallery-item">
+            <a href="#" data-toggle="modal" data-target="#imageModal">
+                <img src="{{ asset('assets/img/img1.jpg') }}" alt="Gallery Image 1">
+            </a>
+        </div>
+        <div class="gallery-item">
+            <a href="#" data-toggle="modal" data-target="#imageModal">
+                <video width="400" height="550" controls>
+                    <source src="{{ asset('assets/video/video-1.mp4') }}" type="video/mp4">
+                </video>
+            </a>
+        </div>
+        <div class="gallery-item">
+            <a href="#" data-toggle="modal" data-target="#imageModal">
+                <video width="400" height="550" controls>
+                    <source src="{{ asset('assets/video/video-3.mp4') }}" type="video/mp4">
+                </video>
+            </a>
+        </div>
+        <div class="gallery-item">
+            <a href="#" data-toggle="modal" data-target="#imageModal">
+                <img src="{{ asset('assets/img/img2.jpg') }}" alt="Gallery Image 1">
+            </a>
+        </div>
+        <div class="gallery-item">
+            <a href="#" data-toggle="modal" data-target="#imageModal">
+                <video width="400" height="550" controls>
+                    <source src="{{ asset('assets/video/video-4.mp4') }}" type="video/mp4">
+                </video>
+            </a>
+        </div>
+    </section>
+
+    {{-- <!-- Modal -->
+    <div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img src="{{ asset('assets/img/sub-3.jpeg') }}" class="img-fluid" alt="Gallery Image">
+                </div>
+            </div>
+        </div>
+    </div> --}}
+    <section class="sub-7">
+        <div class="container">
+            <h2>KAMI AKAN CHECK DULU MASALAH ANDA, JIKA ANDA SETUJU KAMI AKAN GANTI DAN BAIKI DENGAN SEGERA</h2>
+            <div class="content-sub-7">
+                <div class="text-1">
+                    <p>Kami sedia melakukan kerja-kerja EXPRESS seperti sinki tersumbat, paip bocor dan pemasangan paip
+                        baru.</p>
+                </div>
+                <div class="text">
+                    <h1>HUBUNGI SEGERA!</h1>
+                    <a href="http://wasap.my/60172137979">Whatsapp Kami</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="sub-8">
+        <div class="text-top">
+            <h1>STANDARD OPERATION PROCEDURE (SOP)</h1>
+            <h3>Berikut merupakan langkah-langkah yang perlu anda ikut untuk menggunakan perkhidmatan kami.</h3>
+        </div>
+        <div class="text-bottom">
+            <div class="card-1">
+                <img src="{{ asset('assets/img/call-removebg-preview.png') }}" alt="">
+                <h1>1. HUBUNGI KAMI</h1>
+                <p>Sila klik button whatsapp atau call. Beritahu kami lokasi, serta masalah anda. Kami akan cuba consult
+                    anda ditalian dahulu.</p>
+            </div>
+            <div class="card-2">
+                <img src="{{ asset('assets/img/serv-removebg-preview-removebg-preview.png') }}" alt="">
+                <h1>2. ON-SITE SERVIS</h1>
+                <p>Pekerja kami akan terus ke lokasi anda. Kami akan check dulu masalah anda. Kami akan consult anda dan
+                    anda buat keputusan.</p>
+            </div>
+            <div class="card-3">
+                <img src="{{ asset('assets/img/money-removebg-preview.png') }}" alt="">
+                <h1>3. PEMBAYARAN</h1>
+                <p>Setelah anda puas hati dengan perkhidmatan kami, anda boleh buat bayaran secara tunai, kepada pekerja
+                    kami.</p>
+            </div>
+        </div>
+    </section>
+    <section class="sub-9" id="sub-9">
+        <h1>HUBUNGI SEGERA!</h1>
+        <div class="btn-mid">
+            <a href="http://wasap.my/60172137979">Whatsapp Kami</a>
+            <a href="https://mail.google.com/mail/u/0/#all?compose=CllgCKCBkLfmrhtsfRwZPHPGWQKxWJGVLrgsltnTNDmQmsvQFwVSlcKwWJLgMRTTXQtRjKHpQSq"
+                class="btn btn-primary mt-3">Email Kami</a>
+        </div>
+        <div class="img-sub-9">
+            <div class="img-1">
+                <img src="{{ asset('assets/img/100-removebg-preview-1.png') }}" alt="">
+            </div>
+            <div class="img-2">
+                <img src="{{ asset('assets/img/111-removebg-preview-1.png') }}" alt="">
+            </div>
+        </div>
+    </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
